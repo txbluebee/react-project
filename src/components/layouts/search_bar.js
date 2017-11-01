7cef1c0b353f17286c84f5e0a06718610f671aa7
Brian Fan <kuomu.fan@yahoo.com>
2017-10-31 22:04:21 -0700

    set state for search bar

--- src/components/layouts/search_bar.js
+++ src/components/layouts/search_bar.js
@@ -1,16 +1,37 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {term: ''};
+    }
+
+    onInputChange(event){
+        this.setState({ term: event.target.value});
+    }
+
+    onSubmitForm(event){
+        event.preventDefault();
+        console.log(this.state.term);
+    }
     render() {
         return (
-            <div className="row">
+            <div className="row d-flex justify-content-center my-4">
                 <div className="col-lg-6">
-                    <div className="input-group">
-                        <input type="text" className="form-control" placeholder={this.props.placeholders} />
-                        <span className="input-group-btn">
-                            <button className="btn btn-secondary" type="button">Go</button>
-                        </span>
-                    </div>    
+                    <form onSubmit={this.onSubmitForm.bind(this)}>
+                        <div className="input-group">
+                            <input 
+                                type="text" 
+                                className="form-control" 
+                                value={this.state.term}
+                                placeholder={this.props.placeholder} 
+                                onChange={this.onInputChange.bind(this)} />
+                            <span className="input-group-btn">
+                                <button className="btn btn-secondary" type="submit">Go</button>
+                            </span>
+                        </div>    
+                    </form>    
                 </div>    
             </div>
         );
--- src/components/pages/weather_show.js
+++ src/components/pages/weather_show.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './../layouts/header';
 import Navbar from './../layouts/navbar';
+import SearchBar from './../layouts/search_bar';
 
 
 class WeatherShow extends React.Component{
@@ -9,6 +10,7 @@ class WeatherShow extends React.Component{
             <div>
                 <Header title="Weather Forecast"/>
                 <Navbar />
+                <SearchBar placeholder="Search Your City..." />
             </div>
         );
     }
